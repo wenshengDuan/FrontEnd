81535c63c615627fdc267ec9752fc2941e522db4
duanwensheng <824201954@qq.com>
2023-07-13 23:22:36 +0800

    feat:完整版深拷贝

--- JS/DeepClone/index.js
+++ JS/DeepClone/index.js
@@ -7,22 +7,46 @@
  * @param 
  */
 
-const obj = {
+// const obj = {
+//     name: 'mm',
+//     age: 12,
+//     address: {
+//         provice: '湖南',
+//         city: '长沙'
+//     }
+// }
+
+const address = {
+    provice: '湖南',
+    city: '长沙',
+}
+
+// 测试循环引用
+const preson = {
     name: 'mm',
     age: 12,
     address: {
-        provice: '湖南',
-        city: '长沙'
-    }
+        provice: '河北',
+        city: '香河',
+    },
+    list: {}
 }
 
-function deepClone(obj = {}) { 
+preson.belong = preson;
+preson.list.mapList = new Map([['preson', preson]])
+preson.list.setList = new Set([preson])
+
+
+function deepClone(obj = {}, visited = new WeakMap()) { 
     // 如果是基本类型 则直接返回
     if (typeof obj !== 'object' || obj === null) { 
         return obj;
     }
 
     // 处理循环引用
+    if (visited.has(obj)) { 
+        return visited.get(obj);
+    }
 
     // 处理时间类型
     if (obj instanceof Date) { 
@@ -36,22 +60,31 @@ function deepClone(obj = {}) {
     
     // 处理set　map
     if (obj instanceof Set) { 
-
+        const set = new Set();
+        obj.forEach(val => { 
+            set.add(deepClone(val, visited))
+        })
+        visited.set(obj, set)
+        return set;
     }
 
     if (obj instanceof Map) { 
-
-        
+        const map = new Map();
+        obj.forEach((val, key) => { 
+            map.set(key, deepClone(val, visited))
+        })
+        visited.set(obj, map)
+        return map;
     }
     // Math 不用处理因为结果是个数字类型
 
     let target = Array.isArray(obj) ? [] : {}
-
-
+    
+    visited.set(obj, target)
 
     for (let key in obj) { 
         if (obj.hasOwnProperty(key)) {
-            target[key] = deepClone(obj[key])
+            target[key] = deepClone(obj[key], visited)
         }
     } 
 
@@ -59,6 +92,6 @@ function deepClone(obj = {}) {
 }
 
 
-const obj2 = deepClone(obj)
+const obj2 = deepClone(preson)
 
 obj2.address.city = '北京'
