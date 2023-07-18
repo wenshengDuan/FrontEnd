c7b41d8d618122daaf641056a827d61527c4c2fd
duanwensheng <824201954@qq.com>
2023-07-18 18:54:52 +0800

    feat: 手写bind

--- JS/This/index.js
+++ JS/This/index.js
@@ -33,3 +33,35 @@ const lisi = {
         }, 1000)
     }
 }
+
+
+function testBind(...args) { 
+    console.log('testBind-this', this)
+    console.log('args', args)
+
+    return 'change this'
+}
+
+const bind = testBind.bind(null, 1, 2, 3);
+
+
+Function.prototype.mybind = function (...args) { 
+    let [newThis, ...res] = args;
+    const self = this;
+    
+    newThis = newThis ? newThis : window;
+    newThis = typeof newThis === 'object' ? newThis : Object(newThis);
+
+    newThis.fn = self;
+    
+    return function (...args) { 
+        const allArgs = [...res, ...args];
+        const result = newThis.fn(...allArgs);
+        delete newThis.fn;
+        return result;
+    }
+    
+}
+
+// const mybind = testBind.mybind({x: 200}, 4, 5, 6);
+const mybind = testBind.mybind(null, 4, 5, 6);
